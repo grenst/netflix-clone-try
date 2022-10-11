301af2619ae0c9d15f848ffce5c0578a803cbfd1
grenst <74575794+grenst@users.noreply.github.com>
2022-10-11 19:07:40 +0200

    ADDED FETCHING
    -Fatchinf from TMDB
    -Banner with random movie and info from data base
    -Added Rows...

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -2,12 +2,22 @@ import React from "react";
 import "./HomeScreen.css";
 import Nav from "./Nav";
 import Banner from "./Banner";
+import requests from "./Requests";
+import Row from "./Row";
 
 function HomeScreen() {
   return (
     <div className="homeScreen">
       <Nav />
       <Banner />
+
+      <Row title="NETFLIX ORIGINAL" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} isLargeRow />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} isLargeRow />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} isLargeRow />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} isLargeRow />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} isLargeRow />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} isLargeRow />
     </div>
   );
 }
